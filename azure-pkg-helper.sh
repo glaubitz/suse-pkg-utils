28dedc3d86a47c0a4d453631618a5b767763f63f
John Paul Adrian Glaubitz <glaubitz@suse.com>
2017-05-15 11:37:45 +0200

    azure-pkg-helper: Add support for writing LICENSE.txt files

--- azure-pkg-helper.sh
+++ azure-pkg-helper.sh
@@ -34,6 +34,7 @@ function usage() {
 
      -d             path to Azure SDK for Python (required)
      -h             show this help message
+     -l             generate LICENSE files for RPM
      -p             print package information
      -s             generate spec files for RPM
      -v             verbose
@@ -49,10 +50,11 @@ OPT_VERBOSE=0
 OPT_ZIPGEN=0
 
 REMOVE_ARGS=0
-while getopts "d:hpsvz" opt ; do
+while getopts "d:hlpsvz" opt ; do
     case "$opt" in
 	d) OPT_AZURE_DIR="$OPTARG" ; REMOVE_ARGS="$((REMOVE_ARGS + 2))" ;;
         h) usage ;;
+	l) OPT_LICENSEGEN="1" ; REMOVE_ARGS="$((REMOVE_ARGS + 1))" ;;
 	p) OPT_PRINT="1" ; REMOVE_ARGS="$((REMOVE_ARGS + 1))" ;;
 	s) OPT_SPECGEN="1" ; REMOVE_ARGS="$((REMOVE_ARGS + 1))" ;;
         v) OPT_VERBOSE="1" ; REMOVE_ARGS="$((REMOVE_ARGS + 1))" ;;
@@ -162,6 +164,56 @@ install -m 644 %{SOURCE1} %{_builddir}/$PACKAGE-%{version}
 
 %changelog
 EOF
+	    fi
+
+	    if [ $OPT_LICENSEGEN == "1" ] ; then
+		echo "Writing LICENSE.txt for file $PACKAGE ..."
+		case $LICENSE in
+		    MIT*)
+			cat > $TARGET/python-$PACKAGE/LICENSE.txt <<EOF
+The MIT License (MIT)
+
+Copyright (c) 2016 Microsoft Corporation. All rights reserved.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+EOF
+			;;
+		    Apache*2.0*)
+			cat > $TARGET/python-$PACKAGE/LICENSE.txt <<EOF
+Copyright (c) 2016 Microsoft Corporation. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+EOF
+			;;
+		    *)
+			echo "Warning: Unknown license for package $PACKAGE."
+		esac
 	    fi
 
 	    if [ $OPT_ZIPGEN == "1" ] ; then
