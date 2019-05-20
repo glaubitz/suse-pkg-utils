a8ef6f02f6ee0f7061ea47fd14e0076dbb741af9
John Paul Adrian Glaubitz <glaubitz@suse.com>
2019-05-20 14:14:42 +0200

    azure-sdk-pkg-helper: Update copyright year in generated spec file

--- azure-sdk-pkg-helper.sh
+++ azure-sdk-pkg-helper.sh
@@ -170,7 +170,7 @@ if [ "$OPT_AZURE_DIR" ] && [ -d "$OPT_AZURE_DIR" ] ; then
 #
 # spec file for package python-$PACKAGE
 #
-# Copyright (c) 2018 SUSE LINUX GmbH, Nuernberg, Germany.
+# Copyright (c) 2019 SUSE LINUX GmbH, Nuernberg, Germany.
 #
 # All modifications and additions to the file contributed by third parties
 # remain the property of their copyright owners, unless otherwise agreed
